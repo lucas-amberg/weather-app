47b8cc803fc923e92af6d81284843ca83a58f515
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-24 16:09:40 -0800

    Initialize Next.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# weather-app
-A functional weather app I created using Next.js
+# Weather App
+## About the project
+This is a weather app I created over a few days to test my skills with the
+Next.js framework.
+## Upcoming Features
+* Search and view every city with weather data
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Weather App',
+  description: 'A new weather app with all the great features you love.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
