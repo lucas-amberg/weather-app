987b814fd7c99132e40eb206979a46176b0c62c6
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-25 16:47:21 -0800

    Add styles for homepage on other screen sizes

--- app/ui/homepage.tsx
+++ app/ui/homepage.tsx
@@ -12,8 +12,8 @@ export default function Homepage() {
   const homepageImageSrc = getRandomImage();
 
   return(
-      <div className="flex flex-col items-center justify-center w-5/6 h-400px shadow-lg h-full rounded-lg bg-gray-100">
-        <h1 className='text-xl font-bold'>
+      <div className="flex flex-col gap-5 items-center justify-center w-5/6 h-400px shadow-lg h-full rounded-lg bg-gray-100">
+        <h1 className='text-xl font-bold md:text-3xl'>
           Next.js Weather App
         </h1>
         <Image
@@ -21,8 +21,9 @@ export default function Homepage() {
           width={100}
           height={100}
           alt='Homepage Image'
+          className='md:w-36'
         />
-        <div className='text-sm'>
+        <div className='text-sm md:w-lg'>
           Created by <Link className='text-blue-600' href={'https://lucasamberg.dev/'}>Lucas Amberg</Link>
         </div>
       </div>
--- app/ui/instructions.tsx
+++ app/ui/instructions.tsx
@@ -1,10 +1,10 @@
 export default function Instructions() {
   return(
   <div className="flex flex-col items-center justify-center w-5/6 h-400px shadow-lg h-full rounded-lg bg-gray-100">
-    <h1 className='text-xl font-bold'>
+    <h1 className='text-xl font-bold md:text-3xl'>
       How to Use:
     </h1>
-    <ol className='text-sm p-6'>
+    <ol className='text-sm md:text-md p-6'>
       <li className="m-3">1.&ensp;Type in the name of a city in the search bar at the top of the screen.</li>
       <li className="m-3">2.&ensp;Wait for the results to load...</li>
       <li className="m-3">3.&ensp;Success!</li>
