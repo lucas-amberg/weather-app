f981d91a5dd0bd8afc54bbc34ae78090288296b5
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-01-14 18:21:19 -0800

    Update footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return(
     <div className="h-16 text-xs bg-white items-center justify-center flex flex-col">
       <a href="https://lucasamberg.dev">&copy; Lucas Amberg 2023 All Rights Reserved</a>
-      <a href="https://github.com/lucas-amberg/weather-app">Release v1.1</a>
+      <a href="https://github.com/lucas-amberg/weather-app">Release v1.1 (Pre-Release)</a>
     </div>
   )
 }
